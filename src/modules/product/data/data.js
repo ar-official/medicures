92406e2aba9aa700ee9641c1f5cb70cb7143f129
amangrw <39518200+amangrw@users.noreply.github.com>
2022-08-28 15:46:22 +0530

    changes

---
+++ src/modules/product/data/data.js
@@ -0,0 +1,143 @@
+export const data = {
+  "fresh-recommendation": [
+    {
+      "id": 1,
+      "name": "APPLE iPhone 11 (Black, 64 GB)",
+      "description": "64 GB ROM 15.49 cm (6.1 inch) Liquid Retina HD Display 12MP + 12MP | 12MP Front Camera A13 Bionic Chip Processor",
+      "rate": "3000"
+    }
+  ],
+  "today-deal": [
+    {
+      "id": 1,
+      "body": "some comment",
+      "postId": 1
+    }
+  ],
+  "offer-zone": {
+    "name": "typicode"
+  },
+  "product": [
+    {
+      "id": 1,
+      "item_id": '14805',
+      "name": "Flekosteel",
+      "description": "NO local celebrities on creatives (landings, prelandings and banners) Delivery zones: Dushanbe, Khudzhand, Istaravshan, Pendzhikent, Kurgan-Tyube, Kulyab, Nurek, Yavan, Vakhdat, Rogun, Faizabad, Tursunzade, Gissar, Rudaki, Shakhrinav.",
+      "rate": "250 TJS",
+      "MRP": "500 TJS",
+      "tracking_url": "https://uh8d10ad74uh.axdsz.pro/?target=-7EBNQCgQAAAfe9wED1TkABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/goods_126/Tube.png",
+      "avail_country_code": ["TJ"],
+      "avail_country": ["Tajikistan"],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 2,
+      "item_id": '35053',
+      "name": "Slender Quick",
+      "description": "weightloss",
+      "rate": "7990 ARS",
+      "MRP": "15980 ARS",
+      "tracking_url": "https://uh8d10ad74uh.wsjksz.cc/?target=-7EBNQCgQAAAfe9wED7YgABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/35053/Caja_slender.png",
+      "avail_country_code":["AR"],
+      "avail_country": ["Argentina"],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 3,
+      "item_id": '32791',
+      "name": "Keto Bullet",
+      "description": "Keto Bullet Coffee is a drink for weight loss. Have you ever heard about Keto Coffee? If not, you missed a lot. Keto Coffee is a real healthy trend in the last few years. It combines not only excellent taste but also great benefits in terms of your body weight, well-being, appearance, and even brain work! You ask how can usual coffee influence us so much? The answer is simple. The combination of coffee and coconut oil makes a great advantage. First, coconut oil makes you feel full longer. Second, coffee increases metabolism. Third, coconut oil containing medium-chain triglycerides protects your brain, improves cognitive abilities, and can be a preventive measure against Alzheimer's disease! Isn't it enough to start changing life for the better? We think it is. Also, Keto Coffee is spread worldwide and really effective. You have a great chance to run the offer now and get your profit (to tell you the truth conversion rate is just GREAT). So, don't waste time and start today! Active ingredients: Instant Coffee, Chicory, MCT oil, Coconut Pulp Extract, Chromium Picolinate. NO local celebrities on creatives (landings, prelandings and banners) Please send traffic directly to landing pages, not via API. If you run via API, please set up second page. Every Landing Page promoting food supplements must contain the next footnotes: *This Advertorial is based on fiction and any similarity to real people, places, or events are purely coincidental. *The product advertised is a dietary supplement. Dietary supplements are products intended to supplement the diet. They are not medicines and are not intended to treat, diagnose, mitigate, prevent, or cure diseases.It is prohibited:- any kind of mentioning medical/health workers, medical facilities, patients, and their comments is prohibited;- the images of people who might resemble a medical worker;- the statements that denigrate traditional medicine and allopathic medicines;- attributing to food supplements the property of preventing, treating, or curing a human disease or refer to such properties;- include direct statements or suggest that a varied and balanced diet cannot provide adequate amounts of nutrients in general. Food supplements are administered to maintain health, to promote the growth and development of the body.- to guarantee an effect- to guarantee the absence of side effects This offer works on Hybrid model. It’s the unique model that combines COD and SS to increase the chances of affiliates to get a higher CR. When a user intends to place an order, they can decide upon whether to pay in cash or online, by card. In case, the user leaves the payment page, a call center agent will contact them to finish the sale. You can also double your profit by launching extra campaigns, utilizing creative pairs for Hispanic traffic, i.e., for Spanish-speaking citizens who live in America. The campaigns in Spanish often have a higher CR, and, what's more, such users prefer to pay in cash.",
+      "rate": "49 $",
+      "MRP": "98 $",
+      "tracking_url": "https://uh8d10ad74uh.wsjksz.cc/?target=-7EBNQCgQAAAfe9wEDF4AABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/32791/003-3.jpg",
+      "avail_country_code":["US"],
+      "avail_country": ['United States'],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 4,
+      "item_id": '33634',
+      "name": "Hyperdrops ",
+      "description": " Improve blood circulation, so it is used to help relieve the symptoms of initial or advanced arteriosclerosis - Rejuvenates blood vessels and makes them more elastic - Lowers high blood pressure - Lowers the level of triglyceride and cholesterol in the blood - Beneficial effect on the heart functions - Cleanses blood, gallbladder and liver from various toxins - Stimulates the immune system and is used as a means to reduce the risk of viral and bacterial infections - Accelerates digestion - Acts against parasites in the intestine and prevents infectious inflammation of the intestinal mucosa - Cleanses the organism of harmful elements caused by unhealthy habits such as drinking alcohol and cigarette consumption or improper diet - Slows down the ageing process of the cell by binding and expelling free radicals in the cell (antioxidant effect) - It is rich in vitamin C, essential oil, carotene and proteins",
+      "rate": "49 EUR",
+      "MRP": "",
+      "tracking_url": "https://uh8d10ad74uh.wsjksz.cc/?target=-7EBNQCgQAAAfe9wEDYoMABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/33634/hyperdrops.jpg",
+      "avail_country_code":["ES"],
+      "avail_country": ['Spain'],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 5,
+      "item_id": '',
+      "name": "Dermal ",
+      "description": "Intended for the treatment of psoriasis, chronic eczema and other skin changes accompanied by inflammation. Not recommended for people allergic to any of the ingredients. NO local celebrities on creatives (landings, prelandings and banners)",
+      "rate": "39 EUR",
+      "MRP": "78 EUR",
+      "tracking_url": "https://uh8d10ad74uh.axdsz.pro/?target=-7EBNQCgQAAAfe9wEDyXIABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/29385/Dermal.png",
+      "avail_country": ['Kosovo'],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 6,
+      "item_id": '9218',
+      "name": "Inno Gialuron",
+      "description": "Anti-aging booster with hyaluronic acid. The hyaluronic acid provides deep skin hydration, contributing to diminishing lines. Biotechnologically modified form of folic acid increases the rejuvenating effect reducing wrinkles and protecting the skin from environmental stress factors. Rich in polysaccharides, the extracts of fucus and kelp have soothing and antioxidant action. Regular use of the booster makes the skin smooth and radiant!",
+      "rate": "34990 CLP",
+      "MRP": "",
+      "tracking_url": "https://uh8d10ad74uh.axdsz.pro/?target=-7EBNQCgQAAAfe9wEDAiQABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/9218/gilaruon.png",
+      "avail_country": ['Chile'],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 7,
+      "item_id": '33609',
+      "name": "Hyaluronan ",
+      "description": "An antiaging cream with Hyaluronic Acid and Dragon's blood",
+      "rate": "7990ARS",
+      "MRP": "15980ARS",
+      "tracking_url": "https://uh8d10ad74uh.wsjksz.cc/?target=-7EBNQCgQAAAfe9wEDSYMABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/33609/pomo_solo.png",
+      "avail_country": ['Argentina'],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 8,
+      "item_id": '32536',
+      "name": "Neoritm - PH",
+      "description": "Neoritm Сapsules - provide a carefully balanced complex that protects blood vessels and maintains heart health.The unique formula gently and effectively supports the entire cardiovascular system and general well-being. The Magnesium in the formula normalizes the heart rate and reduces the risk of blood clots. The L-carnitine in the capsules supports the body’s energy levels and improves physical endurance. The addition of Calcium helps to maintain the strength of blood vessels and healthy levels of cholesterol. Neoritm Capsules, your heart’s best friend! Product form/ Packaging: 20 capsules inside a plastic jar. Active ingredients: Magnesium, Calcium and L-carnitine. Mode of application: Intake by mouth with water 1 capsule, twice a day, with food.",
+      "rate": "1990 ₱",
+      "MRP": "3980 ₱",
+      "tracking_url": "https://uh8d10ad74uh.wsjksz.cc/?target=-7EBNQCgQAAAfe9wEDGH8ABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/32536/image-2021-05-27-12-03-35-441.png",
+      "avail_country": ['Philippines'],
+      "meta_title": '',
+      "meta_description": ''
+    },
+    {
+      "id": 9,
+      "item_id": '30428',
+      "name": "Rechiol - CL",
+      "description": "Anti-aging serum. Active ingredient of Rechiol - bakuchiol – is a plant-based substitute of retinol. This phyto concentrate is produced from a rare plant Psoralea corylifolia in India. Bakuchiol enables production of types I, III and IV collagen and suppresses enzymes that disturb protein and cellular metabolism of the skin. It doesn’t only affect the surface of the epidermis, but also penetrates into the deep layers of the derm, enabling cellular renewal mechanism from inside. Active ingredients: Bakuchiol, PEPHA®-TIGHT, Collagen, Almond oil, Vitamin C, Licorice extract. NO local celebrities on creatives (landings, prelandings and banners).",
+      "rate": "34990 CLP",
+      "MRP": "69980 CLP",
+      "tracking_url": "https://uh8d10ad74uh.wsjksz.cc/?target=-7EBNQCgQAAAfe9wED3HYABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx",
+      "img": "https://my.adcombo.com/offer_preview/30428/Skype_Picture_2020_02_26T13_13_23_334Z.jpeg",
+      "avail_country": ['Chile'],
+      "meta_title": 'medicures: Rechiol-cl-wrinkle-remover',
+      "meta_description": ''
+    }
+  ]
+}
