6ad1055cc25495f625791950ef4f756fc9a7c0c6
ar-official <ar.coolstuffofficial@gmail.com>
2022-09-04 18:40:09 +0530

    Updates

---
+++ precache-manifest.35d86d3b508a9541671315ef096c954c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9026627c5397921386ec",
+    "url": "./static/css/main.db8b5052.chunk.css"
+  },
+  {
+    "revision": "9026627c5397921386ec",
+    "url": "./static/js/main.9026627c.chunk.js"
+  },
+  {
+    "revision": "ce47ab81b35290e2776b",
+    "url": "./static/js/1.ce47ab81.chunk.js"
+  },
+  {
+    "revision": "87cf47ccbfb799c72fe9",
+    "url": "./static/css/2.1acd4f5a.chunk.css"
+  },
+  {
+    "revision": "87cf47ccbfb799c72fe9",
+    "url": "./static/js/2.87cf47cc.chunk.js"
+  },
+  {
+    "revision": "a743834eda8e97d8e24e",
+    "url": "./static/js/runtime~main.a743834e.js"
+  },
+  {
+    "revision": "84beb6ca4834906a4d0fbced2d750d15",
+    "url": "./static/media/product1.84beb6ca.jpg"
+  },
+  {
+    "revision": "1ecf349c2433a408cee867d1481afbe9",
+    "url": "./static/media/logo.1ecf349c.PNG"
+  },
+  {
+    "revision": "81931a101cbc01da08fc8e2876364463",
+    "url": "./static/media/product2.81931a10.jpg"
+  },
+  {
+    "revision": "5b04ebe429a07939f338e610857f8894",
+    "url": "./static/media/product3.5b04ebe4.jpg"
+  },
+  {
+    "revision": "03487d6eaa2d0871e4e9ca850d30f7b5",
+    "url": "./static/media/product4.03487d6e.jpg"
+  },
+  {
+    "revision": "fbbe683edabe82c9f8e166eb7916abdb",
+    "url": "./index.html"
+  }
+];
